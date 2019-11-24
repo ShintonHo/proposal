f7fa3cce1ee8a1672a31812d5f7618284b75fd1f
ShintonHo <hexitong1@gmail.com>
2019-11-24 18:23:07 -0500

    upload

---
+++ config.js
@@ -0,0 +1,265 @@
+var config = {
+    style: 'mapbox://styles/khysn/ck38222z00v2m1cpjrghx7ktx',
+    accessToken: 'pk.eyJ1Ijoia2h5c24iLCJhIjoiY2pnbGJubDdoMDBmbjJ3bzFzdGg1amhrOSJ9.invAhppOjjNxmU5IeF4kwg',
+    showMarkers: false,
+    theme: 'light',
+    alignment: 'left',
+    title: 'Exciting Riverfront',
+    subtitle: 'A new proposal for the riverfront park design',
+    footer: 'Copyright &copy; XT 2019',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            title: 'Aims to create coherence of Madison’s incredible array of attractions into a fuller, more complete experience for the city.',
+            // image: './path/to/image/source.png',
+            description: '',
+            location: {
+               center: [-85.40601, 38.74096],
+zoom: 12.99,
+pitch: 6.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+
+
+
+
+{
+            id: 'slug-style-id',
+            title: 'WORTH EVERY MILE',
+            // image: './path/to/image/source.png',
+            description: 'At a nexus of cities, Madison is perfectly located to draw on a variety of populations. A spectacular riverfront will amplify Madison’s regional appeal. The more compelling it is to linger and explore, the more likely people are to travel farther to get away—not just from Louisville, Cincinnati, and Indianapolis but from beyond the two hour weekend drive.',
+            location: {
+               center: [-85.40601, 38.74096],
+zoom: 12.99,
+pitch: 6.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+
+
+{
+            id: 'slug-style-id',
+            title: 'DESERVES A FEW EXTRA HOURS',
+            // image: './path/to/image/source.png',
+            description: 'People who now pop over for a day trip—or, like Clinton and his family, who come into town for a single event—will choose to stay the weekend or venture down the hill to explore the waterfront before heading home.',
+            location: {
+               center: [-85.40601, 38.74096],
+zoom: 12.99,
+pitch: 6.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+
+
+
+{
+            id: 'slug-style-id',
+            title: 'EXISTING FRAMEWORK',
+            // image: './path/to/image/source.png',
+            description: 'If at one time the riverfront made Madison a safe place to stop and refuel before launching into the next leg of a journey, it now promises to make Madison a safe place to stop and get one’s bearings before resuming the march of daily life. The existing grid of Madison has created the perfect framework of connectivity that is an opportunity for strengthening the city beyond Main Street',
+            location: {
+               center: [-85.40601, 38.74096],
+zoom: 12.99,
+pitch: 6.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+
+
+
+{
+            id: 'slug-style-id',
+            title: 'MORE THAN A WAY STATION',
+            // image: './path/to/image/source.png',
+            description: 'Scattered through out Madison are the stories from the largest Historical District in America. Historians, architecture aficionados, train enthusiasts, underground railway buffs, and more offer a layer of tourism that can be captured and surprised by the development of the waterfront, showcasing Madison today woven with Madison of the past.',
+            location: {
+               center: [-85.40601, 38.74096],
+zoom: 12.99,
+pitch: 6.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+
+
+
+{
+            id: 'slug-style-id',
+            title: 'CONNECTIVITY',
+            // image: './path/to/image/source.png',
+            description: 'Madison is currently set with a series of unconnected destinations (Parks, Main Street, Historic Architecture, entertainment, etc.). The steet grid provides a wonderful opportunity for building connectivity and clear circulation patterns for visitors in the future.',
+            location: {
+               center: [-85.40601, 38.74096],
+zoom: 12.99,
+pitch: 6.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+
+
+
+{
+            id: 'slug-style-id',
+            title: 'COHESIVE RIVERFRONT',
+            // image: './path/to/image/source.png',
+            description: 'The first step to creating a connected city is going back to the origins of the city of Madison and creating a strong waterfront. Building upon all of the existing destinations and events that take place along the river and re-envisioning it as one space, will create a cohesive riverfront with a wide range of activities for everyone.',
+            location: {
+               center: [-85.40601, 38.74096],
+zoom: 12.99,
+pitch: 6.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+
+
+
+{
+            id: 'slug-style-id',
+            title: 'EAST-WEST MOVEMENT',
+            // image: './path/to/image/source.png',
+            description: 'With the creation of a unified park, the city of Madison is set up with strong east west movement (Riverfront-Vaughn Drive, Main Street, West 3rd Street, etc.). This establishes a clear framework for the city to build upon.',
+            location: {
+               center: [-85.40601, 38.74096],
+zoom: 12.99,
+pitch: 6.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+
+
+
+{
+            id: 'slug-style-id',
+            title: 'WHERE EVERYONE COMES TO STAY',
+            // image: './path/to/image/source.png',
+            description: 'Building off the energy of a new Riverfront Park, north-south fingers will become the next critical phases to unite the core of Madison to the water. The outcome will re-energize existing businesses and city economy by making it easier for people to move from one destination to the next.',
+            location: {
+               center: [-85.40601, 38.74096],
+zoom: 12.99,
+pitch: 6.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+
+
+
+
+
+    ]
+};
